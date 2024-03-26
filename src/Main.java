9a1457f7ac4b4af1b0351f1175eb0b1308b55ffb
VasiliyIgnatov <165067277+VasiliyIgnatov@users.noreply.github.com>
2024-03-26 21:42:24 +0300

    dev

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.File;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicLong;
 
+public class Main {
+    public static void main(String[] args) throws Exception {
+        String folderPath = "/home/vasiliy/lib";
+        File file = new File(folderPath);
+
+        System.out.println(getFolderSize(file));
+
+
+    }
+
+    public static long getFolderSize(File folder) throws Exception {
+        if (folder.isFile()) {
+            return folder.length();
+        }
+        long sum = 0;
+        File[] files = folder.listFiles();
+        for (File file : files) {
+            sum += getFolderSize(file);
+        }
+        return sum;
     }
 }
